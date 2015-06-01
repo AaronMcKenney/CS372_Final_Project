35d747af4bebac5819934d586d1fad2f4793bf1f
Chris Newman <cnewman.robozinc@gmail.com>
2015-05-31 23:41:41 -0700

    Client can now message server

--- client.py
+++ client.py
@@ -2,13 +2,14 @@ import sys
 import socket
 
 def main():
-	if len(sys.argv) < 2:
-		print 'Enter the IP address for the server.'
-		exit()
+    if len(sys.argv) < 2:
+        print 'Enter the IP address for the server.'
+        exit()
 
-	saddr = sys.argv[1]
-	csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	csocket.connect((saddr, 80))
+    saddr = sys.argv[1]
+    csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    csocket.connect((saddr, 80))
+    csocket.send("Why hello there!")
 
 if __name__ == '__main__':
 	main()
--- server.py
+++ server.py
@@ -2,19 +2,17 @@ import sys
 import socket
 
 def main():
-	#if len(sys.argv) < 2:
-	#	print 'Enter the IP address for the server.'
-	#	exit()
 
-	#ip_addr = sys.argv[1]
+    ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssocket.bind(('', 80))
+    print ssocket.getsockname()
+    ssocket.listen(5)
 
-	ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ssocket.bind(('', 80))
-	ssocket.listen(5)
-
-	while 1:
-		(csocket, caddr) = ssocket.accept()
-		print caddr
+    while 1:
+        (csocket, caddr) = ssocket.accept()
+        print caddr
+        message = csocket.recv(1024)
+        print message
 
 if __name__ == '__main__':
 	main()
