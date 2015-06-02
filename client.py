17bd8ad6cb81bebcf1b454e58acd51967fbd8d22
Chris Newman <cnewman.robozinc@gmail.com>
2015-06-01 18:51:07 -0700

    Implemented simple Lounge with multiple clients
    Unexpected client disconnections cause crashes for now.

--- client.py
+++ client.py
@@ -1,15 +1,36 @@
 import sys
 import socket
+import string
 
 def main():
     if len(sys.argv) < 2:
-        print 'Enter the IP address for the server.'
+        print('Enter the IP address for the server.')
         exit()
 
     saddr = sys.argv[1]
     csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     csocket.connect((saddr, 80))
-    csocket.send("Why hello there!")
+
+    while(1):
+        message = csocket.recv(1024)
+        print(message)
+        break
+
+    #Enter lounge
+    if(message == "Connected"):
+        response = "N"
+        while(response != "Y" and response != "y"):
+            response = raw_input("Ready to begin? (Y/N) ")
+        csocket.send("Ready")
+
+    while(1):
+        message = csocket.recv(1024)
+        print(message)
+        break
+
+        #Close connection for now
+        #csocket.send("Close")
+        #exit()
 
 if __name__ == '__main__':
 	main()
--- server.py
+++ server.py
@@ -1,18 +1,64 @@
 import sys
 import socket
+import thread
+
+#Global vars
+numConnections = 0
+ready = 0
+connections = []
+
+def clientThread(csocket, caddr):
+    #Recieve client messages
+    while 1:
+        message = csocket.recv(1024)
+
+        #Client (proper) disconnect
+        if message == "Close":
+            csocket.close()
+
+            #Close if all connections lost
+            global numConnections
+            numConnections -= 1
+            if numConnections < 1:
+                break
+
+        #Client ready to begin
+        if message == "Ready":
+            print(caddr[0] + " is ready!")
+            global ready
+            ready += 1
+
+            #If everyone is ready, start the game
+            if(ready == numConnections):
+                csocket.send("The game has begun!")
+
 
 def main():
-
     ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssocket.bind(('', 80))
-    print ssocket.getsockname()
-    ssocket.listen(5)
+    try:
+        ssocket.bind(('', 80))
+    except socket.error as msg:
+        print("Bind failed. Error code: " + str(msg[0]))
 
+    ssocket.listen(5)
+    print("Server ready")
+
+    #Accept new connections
     while 1:
         (csocket, caddr) = ssocket.accept()
-        print caddr
-        message = csocket.recv(1024)
-        print message
+
+        #Add new connection to global list
+        global connections
+        connections.append((csocket, caddr))
+
+        print("New connection: " + caddr[0])
+        csocket.send("Connected")
+
+        #Count new client in lounge
+        global numConnections
+        numConnections += 1
+
+        thread.start_new_thread(clientThread, (csocket,caddr))
 
 if __name__ == '__main__':
 	main()
