31114ef9e8c56d85e7f119b2e9395d523c836926
Chris Newman <cnewman.robozinc@gmail.com>
2015-06-02 19:20:38 -0700

    Lounge uses server thread, client thread, conditional variable

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
 import sys
 import socket
 import thread
+import threading
 
 #Global vars
 numConnections = 0
 ready = 0
+everyoneReady = threading.Condition()
 connections = []
+beginGame = 0
+
+def serverThread(ssocket):
+    while 1:
+        (csocket, caddr) = ssocket.accept()
+        global numConnections
+        numConnections += 1
+        if beginGame == 0:
+            #We are in the lobby
+            global connections
+            connections.append((csocket, caddr))
+            print("New connection: " + caddr[0])
+            csocket.send("Connected")
+            thread.start_new_thread(clientThread, (csocket, caddr))
 
 def clientThread(csocket, caddr):
     #Recieve client messages
     while 1:
         message = csocket.recv(1024)
 
-        #Client (proper) disconnect
-        if message == "Close":
+        #Client disconnect
+        if message == "Close" or message == 0:
             csocket.close()
 
             #Close if all connections lost
@@ -28,10 +44,11 @@ def clientThread(csocket, caddr):
             global ready
             ready += 1
 
-            #If everyone is ready, start the game
-            if(ready == numConnections):
-                for client in connections:
-                    client[0].send("The game has begun!")
+            if ready == numConnections:
+                global everyoneReady
+                everyoneReady.acquire()
+                everyoneReady.notifyAll()
+                everyoneReady.release()
 
 
 def main():
@@ -44,22 +61,22 @@ def main():
     ssocket.listen(5)
     print("Server ready")
 
-    #Accept new connections
-    while 1:
-        (csocket, caddr) = ssocket.accept()
+    thread.start_new_thread(serverThread, (ssocket,))
 
-        #Add new connection to global list
-        global connections
-        connections.append((csocket, caddr))
+    #If everyone is ready, start the game
+    global everyoneReady
+    everyoneReady.acquire()
+    while(ready != numConnections or numConnections == 0):
+        everyoneReady.wait()
+    everyoneReady.release()
 
-        print("New connection: " + caddr[0])
-        csocket.send("Connected")
+    for client in connections:
+        client[0].send("The game has begun!")
+        global beginGame
+        beginGame = 1
 
-        #Count new client in lounge
-        global numConnections
-        numConnections += 1
-
-        thread.start_new_thread(clientThread, (csocket,caddr))
+    print("Ready set go!")
+    quit()
 
 if __name__ == '__main__':
 	main()
